5f460d39557ebb3381943c9af8c117ae75fae1c1
atiilla <9992685+atiilla@users.noreply.github.com>
2023-08-20 00:28:35 +0200

    cookie

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
         date: new Date(),
         ip: remoteIp,
         usergent: req.headers['user-agent'],
-        cookie: req.headers['dotcom_user'],
+        cookie: req.cookies['dotcom_user'],
     }
     console.log(logObj);
     const log = new logModel(logObj);
